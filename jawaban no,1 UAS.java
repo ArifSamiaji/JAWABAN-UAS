9decd7856c969b89ee836c4a7252c8da67cbec20
ArifSamiaji <103028935+ArifSamiaji@users.noreply.github.com>
2022-06-23 20:51:36 +0700

    Add files via upload

---
+++ jawaban no,1 UAS.java
@@ -0,0 +1,6 @@
+Menentukan data dengan nama SIGRA dengan cara mencari node
+Pertama jika data belum disorting, terlebih dahulu harus melakukan sorting data 
+tersebut menggunakan teknik sorting seperti bubble sort.
+tambahkan codingan BST pada codingan yang kita buat. Supaya kita bisa 
+mencari data SIGRA yang ingin kita cari menggunakan BST 
+maka root akan otomatis mencari data yang kita masukan, karena kita menggunakan BST
---
+++ jawaban no.2 UAS.java
@@ -0,0 +1,73 @@
+prosesnya mulai  dari atas int i lalu turun ke bawah.
+ke int this data untuk memproses sebuah hasil lalu 
+ke tengah ke int ++ untuk menghasilkan sebuah data
+data nilai = {2,18,1,0,56,76,98,34,12}
+length = 8
+urutan saat p = 3?
+
+pivot = 9
+
+#i = 0-1 = -1       j=0
+if(this.data[j] < pivot)
+if(7 < 9)
+i = -1 + 1 = 0
+t = this.data[i]    t = 7
+this.data[0] = this.data[0]
+this.data[0] = this
+
+#i = 0  j = 1
+if(this.data[j] < pivot)
+if(45 < 9)
+t = this.data[i+1]  t = this.data[1]
+this.data[1] = 9
+9 = this
+
+
+##p = 1
+    pivot = 7
+
+#i = 0 - 1 = -1 j = 0
+if(7 < 7)
+i = 1 + 1 = 2
+
+#i = 2  j = 3
+if(244 < 7)
+t = this.data[i+1]  t = 255
+this.data[3] = this.data[end]
+this.data[end] = this
+45 = t                                                             {2,18,1,0,56,76,98,34,12}
+
+##p = 3
+    pivot = 255
+
+#i = 1  j=0
+if(7 < 255)
+i = 1 = 1 = 2
+                                                                    {2,18,1,0,56,76,98,34,12}
+#i = 2  j = 1
+if(1 < 255)
+i = 2 + 1 = 3
+
+#i = 3  j = 2
+if(1 < 255)
+i = 3 + 1 = 4
+
+#i = 4  j = 3
+if(1 < 255)
+i = 3 + 1 = 5
+
+#i = 5  j = 4
+if(1 < 255)
+i = 4 + 1 = 5
+
+#i = 6  j = 5
+if(1 < 255)
+i = 5 + 1 = 6
+
+#i = 7  j = 6
+if(1 < 255)
+i = 6 + 1 = 7
+
+#i = 8  j = 7
+if(1 < 255)
+i = 7 + 1 = 8
